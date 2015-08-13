f6cecd0b34f016bfac7a34c9da023532e866ec59
pcrane <pcrane@goscoutgo.com>
2015-08-13 18:14:42 -0400

    fixed counthouse/adventure modalClose call. added noisier debug output

--- tmp.js
+++ tmp.js
@@ -43,7 +43,7 @@ function wrap_next(fn, next) {
 }
 // wrapper function to set a delay
 function wait_a_bit(delay_ms, next) {
-    console.info('setting timeout',delay_ms,'for',next);
+    if(auto_debug) {console.debug('setting timeout',delay_ms,'for',next)};
     setTimeout(next, delay_ms);
 }
 var wait_fn = _.partial(wait_a_bit, 2000);
@@ -63,8 +63,8 @@ function do_finish_production(building, next) {
     doFinishProduction(building.item_id.toString());
     next();
 }
-function close_modal_large(modal_name, next) {
-    closeModalLarge(modal_name);
+function close_modal_large(next) {
+    closeModalLarge('modal_dialogs_top');
     next();
 }
 function click_production_tab(building, next) {
@@ -85,8 +85,9 @@ function click_collect_counthouse(building, next) {
     doCollect(building.item_id.toString());
     next();
 }
-function donezo() {console.info('done');task_running=false;}
+function donezo() {if(auto_debug) {console.debug('donezo')};task_running=false;}
 function check_popups(next) {
+    if(auto_debug) {console.debug('checking popups')};
     if($('#levelmodal').length > 0) closeInterstitial();
     next();
 }
@@ -95,26 +96,34 @@ function check_popups(next) {
 function handle_done_item_production(building, next) {
     // was producing something, but no more build time left means we're Done
     if(building.hasOwnProperty('producing_archetype_id') && !building.hasOwnProperty('build_remaining')) {
+        if(auto_debug) {console.debug('finishing building',building.name)};
+
         var click_building = _.partial(click_building_upgrade_panel, building);
         var finish_production = _.partial(do_finish_production, building);
-        var close_modal = _.partial(close_modal_large, 'modal_dialogs_top');
+        var close_modal = _.partial(close_modal_large);
         var chained = [check_popups, click_building, finish_production, close_modal, check_popups];
         var execute = chain_with_delays(chained, wait_fn, next);
         execute();
-    } else next();
+    } else {
+        if(auto_debug) {console.debug('skipping finish, building',building.name,'is not done')};
+        next();
+    }
 }
 
 //IDLE
 function start_item_production(building, item_index, next) {
     // currently producing something, not idle
     if(building.hasOwnProperty('producing_archetype_id')) {
+        if(auto_debug) {console.debug('skipping start, building',building.name,'is not idle')};
         next();
     } else {
+        if(auto_debug) {console.debug('starting building',building.name)};
+
         var click_building = _.partial(click_building_upgrade_panel, building);
         var click_prod_tab = _.partial(click_production_tab, building);
         var click_prod_tab_item = _.partial(click_production_tab_item, item_index);
         var click_prod_tab_item_start = _.partial(click_production_tab_item_start, building);
-        var close_modal = _.partial(close_modal_large, 'modal_dialogs_top');
+        var close_modal = _.partial(close_modal_large);
 
         var chained = [check_popups, click_building, click_prod_tab, click_prod_tab_item, click_prod_tab_item_start, close_modal, check_popups];
         var execute = chain_with_delays(chained, wait_fn, next);
@@ -124,9 +133,11 @@ function start_item_production(building, item_index, next) {
 
 //COUNTHOUSE
 function collect_counthouse(building, next) {
+    if(auto_debug) {console.debug('handling counthouse@',building.name)};
+
     var click_building = _.partial(click_building_upgrade_panel, building);
     var collect = _.partial(click_collect_counthouse, userContext.buildingsData[0]);
-    var close_modal = _.partial(closeModalLarge, 'modal_dialogs_top');
+    var close_modal = _.partial(close_modal_large);
     var chained = [check_popups, click_building, collect, close_modal, check_popups];
     var execute = chain_with_delays(chained, _.partial(wait_a_bit, 2000), next);
     execute();
@@ -134,11 +145,13 @@ function collect_counthouse(building, next) {
 
 //ADVENTUREPARTY
 function do_adventure_party(next) {
+    if(auto_debug) {console.debug('going on an adventure party')};
+
     var open_keep = _.partial(clickBuildingUpgradePanel, 'keep');
     var party_tab = ssAdventureParty;
     var mark_viewed = _.partial(adventureMarkAllAsViewed, false);
     var send_party = _.partial(adventurePartySend, false);
-    var close_modal = _.partial(closeModalLarge, 'modal_dialogs_top');
+    var close_modal = _.partial(close_modal_large);
     var chained = [open_keep, party_tab, mark_viewed, send_party, close_modal];
     chained = _.map(chained, function(fn) {
         return _.partial(wrap_next, fn);
@@ -154,17 +167,17 @@ closeInterstitial();
 
 function main_loop() {
     if(to_run.length === 0 || user_interaction || task_running) {
-        // sleep for 30s
-        setTimeout(main_loop, 30 * 1000); // 30 seconds
+        if(auto_debug) {console.debug('main loop skipping...')};
     } else {
         // pull a job, then schedule another loop shortly
         var next = to_run.pop();
-        console.info('running', next);
+        if(auto_debug) {console.debug('main loop running',next)};
         next();
-        //setTimeout(main_loop, 1000); // 1 second
     }
+    setTimeout(main_loop, 20 * 1000); // 20 seconds
 }
 function post_loop(fn, delay_ms) {
+    if(auto_debug) {console.debug('post-loop handler called, scheduling', fn)};
     setTimeout(fn, delay_ms);
     task_running=false;
     main_loop();
@@ -176,6 +189,7 @@ function counthouse() {
     collect_counthouse(userContext.buildingsData[building_ids.counting.index], next_loop);
 }
 function schedule_counthouse() {
+    if(auto_debug) {console.debug('adding counthouse to queue')};
     to_run.push(counthouse);
 }
 
@@ -185,6 +199,7 @@ function adventure_party() {
     do_adventure_party(next_loop);
 }
 function schedule_adventure_party() {
+    if(auto_debug) {console.debug('adding adventure party to queue')};
     to_run.push(adventure_party);
 }
 
@@ -197,6 +212,7 @@ function check_godswood() {
     handle_done_item_production(userContext.buildingsData[building_ids.godswood.index], start_item);
 }
 function schedule_check_godswood() {
+    if(auto_debug) {console.debug('adding godswood to queue')};
     to_run.push(check_godswood);
 }
 
@@ -207,6 +223,7 @@ function check_feast() {
     handle_done_item_production(userContext.buildingsData[building_ids.feast.index], start_item);
 }
 function schedule_check_feast() {
+    if(auto_debug) {console.debug('adding feast to queue')};
     to_run.push(check_feast);
 }
 
@@ -217,6 +234,7 @@ function check_sept() {
     handle_done_item_production(userContext.buildingsData[building_ids.sept.index], start_item);
 }
 function schedule_check_sept() {
+    if(auto_debug) {console.debug('adding sept to queue')};
     to_run.push(check_sept);
 }
 
@@ -227,6 +245,7 @@ function check_village() {
     handle_done_item_production(userContext.buildingsData[building_ids.village.index], start_item);
 }
 function schedule_check_village() {
+    if(auto_debug) {console.debug('adding village to queue')};
     to_run.push(check_village);
 }
 
@@ -236,5 +255,6 @@ var user_interaction = false;
 // this doesn't have an escape for error conditions & will deadlock
 // set to true on task start, false on donezo()
 var task_running = false;
+var auto_debug = true;
 var to_run = [counthouse, adventure_party, check_village, check_feast, check_sept, check_godswood];
 main_loop();
